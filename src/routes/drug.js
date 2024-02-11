9b05bff9024a7a685b8a83986c70c59a0ccf9e87
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2024-02-11 19:56:53 +0000

    Add CRUD operations for drug entity

--- src/routes/drug.js
+++ src/routes/drug.js
@@ -2,11 +2,17 @@ import express from 'express';
 import {
   filterDrugController,
   drugDetailController,
+  createDrugController,
+  updateDrugController,
+  deleteDrugController,
 } from '../controllers/drug';
 
 const router = express.Router();
 
 router.get('/', filterDrugController);
 router.get('/:drugId', drugDetailController);
+router.post('/', createDrugController);
+router.put('/:drugId', updateDrugController);
+router.delete('/:drugId', deleteDrugController);
 
 export default router;
